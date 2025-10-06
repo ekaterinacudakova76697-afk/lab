2ebf27fd42b11d57ed9a05d9f7d91fa75b551018
katekatechud <ekaterinacudakova76697@gmail.com>
2025-10-06 14:03:27 +0300

    Update master.py

--- master.py
+++ master.py
@@ -1,1 +1,19 @@
 print("Hello, Boris Igorevich")
+a = int(input())
+b = int(input())
+op = input()
+res = ''
+if op == '+':
+    res = a + b
+elif op == '-':
+    res = a-b 
+elif op == '*':
+    res = a * b
+elif op == '/':
+    if b != 0:
+        res = a / b
+    else: 
+        print("Деление на 0")
+else:
+    print("Ошибка ввода")
+print("Answer:", res)
